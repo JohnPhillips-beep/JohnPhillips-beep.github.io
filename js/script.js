abcabd459a34595af2075bba10003fc4ec24bb91
JohnPhillips-beep <73602065+JohnPhillips-beep@users.noreply.github.com>
2021-01-29 13:00:33 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+        window.setTimeout(function() {
+          document.body.classList.remove('fade');
+        }, 230);
+      });
+    }
+  }
